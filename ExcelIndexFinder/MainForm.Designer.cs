7f5bf539f35f8613924cca8bd61f6a2f3bbb92fe
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2019-01-23 15:19:29 +0100

    Rename form and add program logic

---
+++ ExcelIndexFinder/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ExcelIndexFinder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LabelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBoxInput
+            // 
+            this.TextBoxInput.Location = new System.Drawing.Point(72, 10);
+            this.TextBoxInput.Name = "TextBoxInput";
+            this.TextBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxInput.TabIndex = 0;
+            this.TextBoxInput.TextChanged += new System.EventHandler(this.TextBoxInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Col/Index";
+            // 
+            // LabelResult
+            // 
+            this.LabelResult.AutoSize = true;
+            this.LabelResult.Location = new System.Drawing.Point(178, 13);
+            this.LabelResult.Name = "LabelResult";
+            this.LabelResult.Size = new System.Drawing.Size(37, 13);
+            this.LabelResult.TabIndex = 2;
+            this.LabelResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 41);
+            this.Controls.Add(this.LabelResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LabelResult;
+    }
+}
+
---
+++ ExcelIndexFinder/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelIndexFinder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            var text = ((TextBox)sender).Text;
+            bool isNumeric = int.TryParse(text, out int number);
+            if (isNumeric)
+            {
+                //Give text as result - inefficient but whatever 
+                string currentText = "a";
+                int count = 1;
+                while (TextToIndex(currentText) != number)
+                {
+                    foreach (var x in GetPermutationsWithRept("abcdefghijklmnopqrstuvwxyz", count))
+                    {
+                        currentText = new string(x.ToArray());
+                        if (TextToIndex(currentText) == number)
+                        {
+                            LabelResult.Text = currentText;
+                            break;
+                        }
+                    }
+                    count++;
+                }
+            }
+            else
+            {
+                int result = TextToIndex(text);
+                //Give number as result
+                LabelResult.Text = result.ToString();
+            }
+        }
+
+        public int TextToIndex(string text)
+        {
+            text = text.ToUpper();
+            int length = text.Length;
+            int result = 0;
+            int exponent = length - 1;
+            for (int i = 0; i < length; i++)
+            {
+                int multiplier = text[i] - 'A' + 1;
+                result = result + (int)Math.Pow(26, exponent) * multiplier;
+                exponent--;
+            }
+            return result;
+        }
+
+        static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 0) return new List<List<T>>();
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        static IEnumerable<IEnumerable<T>> GetPermutationsWithRept<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutationsWithRept(list, length - 1)
+                .SelectMany(t => list,
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+    }
+}
